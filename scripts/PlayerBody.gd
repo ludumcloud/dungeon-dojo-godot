6708b87b9f21d08d28bf912f74b5e756090f2e73
Walter Reel <walter.reel@genesys.com>
2019-06-21 18:10:11 -0400

    idle animation

--- scripts/PlayerBody.gd
+++ scripts/PlayerBody.gd
@@ -2,11 +2,10 @@ extends KinematicBody2D
 
 var speed = 400
 var velocity = Vector2()
-onready var sprite = get_node("Pivot/Body")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	sprite.connect("animation_finished", self, "_on_animation_finished")
+	pass
 	
 func get_input():
 	if Input.is_action_pressed("move_left"):
@@ -15,16 +14,13 @@ func get_input():
 		velocity.x = speed
 		
 	if Input.is_action_just_pressed("hi_attack"):
-		sprite.play("attack")
+		pass
 
 	if velocity.x > 0:
 		velocity.x -= 50
 	if velocity.x < 0:
 		velocity.x += 50
 
-
-func _on_animation_finished():
-	sprite.play("idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
