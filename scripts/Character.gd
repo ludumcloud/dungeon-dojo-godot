e70f7faeea49a0adddce954bd920e5d3ed6481f3
Walter Reel <waltercreel3rd@gmail.com>
2019-06-22 10:54:12 -0400

    Simple animation signals test
    Testing using GitHub client on windows.

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -3,11 +3,22 @@ extends KinematicBody2D
 var speed = 400
 var velocity = Vector2()
 
+const IDLE = 'Idle'
+const HIGH_ATTACK = 'HighAttack'
+
 onready var animation_player = get_node("AnimationPlayer")
+
+# Signal Handlers
+func _on_animation_finished(animation):
+	if (animation == IDLE):
+		return
+	else:
+		animation_player.play(IDLE)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animation_player.play("Idle")
+	animation_player.play(IDLE)
+	animation_player.connect("animation_finished", self, "_on_animation_finished")
 	
 func get_input():
 	if Input.is_action_pressed("move_left"):
@@ -16,13 +27,12 @@ func get_input():
 		velocity.x = speed
 		
 	if Input.is_action_just_pressed("hi_attack"):
-		animation_player.play('HighAttack')
+		animation_player.play(HIGH_ATTACK)
 
 	if velocity.x > 0:
 		velocity.x -= 50
 	if velocity.x < 0:
 		velocity.x += 50
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
