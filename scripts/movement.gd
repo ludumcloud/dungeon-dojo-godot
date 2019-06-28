6ae345dfd21d3a249a7e56d50a20e62e6c91f1ec
Walter Reel <walter.reel@genesys.com>
2019-06-28 16:19:06 -0400

    consolidating GD scripts into the attached character script

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -5,6 +5,34 @@ const GRAVITY = 20
 const SPEED = 150
 const JUMP_HEIGHT = -500
 var motion = Vector2()
+onready var animation_tree = get_node("AnimationTree")
+var playback: AnimationNodeStateMachinePlayback
+var active: bool
+
+func _ready():
+	playback = animation_tree.get("parameters/playback")
+	playback.start("Idle")
+	active = true
+
+func _process(delta):
+	if Input.is_action_just_pressed("hi_attack"):
+		playback.travel("HighAttack")
+	if Input.is_action_just_pressed("mid_attack"):
+		playback.travel("MidAttack")
+	if Input.is_action_just_pressed("low_attack"):
+		playback.travel("LowAttack")
+	if Input.is_action_just_released("jump"):
+		playback.travel("Jump")
+	if Input.is_action_just_pressed("damage"):
+		playback.travel("Damage")
+	if Input.is_action_pressed("move_left"):
+		playback.travel("MoveBack")
+	if Input.is_action_pressed("move_right"):
+		playback.travel("MoveForward")
+	if Input.is_action_just_released("move_right"):
+		playback.travel("Idle")
+	if Input.is_action_just_released("move_left"):
+		playback.travel("Idle")
 
 func _physics_process(delta):
 	motion.y += GRAVITY
