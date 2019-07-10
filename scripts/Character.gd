9244f703f0fa3e31db1608752de76f6f46d69660
Mark Abernathy <mark.abernathy@inin.com>
2019-07-10 18:24:52 -0400

    Throw bad state machine into character

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -7,44 +7,81 @@ const JUMP_HEIGHT = -500
 var motion = Vector2()
 onready var animation_tree = get_node("AnimationTree")
 var playback: AnimationNodeStateMachinePlayback
-
+enum states {IDLE, MOVE, ATTACK, AIRBORNE, HURT}
+var current_state
 
 func _ready():
 	playback = animation_tree.get("parameters/playback")
-	playback.start("Idle")
+	playback.start("Jump")
+	current_state = states.AIRBORNE
 
 func _process(delta):
-	if Input.is_action_just_pressed("hi_attack"):
-		playback.travel("HighAttack")
-	if Input.is_action_just_pressed("mid_attack"):
-		playback.travel("MidAttack")
-	if Input.is_action_just_pressed("low_attack"):
-		playback.travel("LowAttack")
-	if Input.is_action_just_released("jump"):
-		playback.travel("Jump")
-	if Input.is_action_just_pressed("damage"):
-		playback.travel("Damage")
-	if Input.is_action_pressed("move_left"):
-		playback.travel("MoveBack")
-	if Input.is_action_pressed("move_right"):
-		playback.travel("MoveForward")
-	if Input.is_action_just_released("move_right"):
-		playback.travel("Idle")
-	if Input.is_action_just_released("move_left"):
-		playback.travel("Idle")
+	# Check if animation returned to idle
+	if playback.get_current_node() == "Idle" :
+		current_state = states.IDLE
+	
+	match current_state:
+		states.IDLE:
+			if Input.is_action_just_pressed("hi_attack"):
+				playback.travel("HighAttack")
+				current_state = states.ATTACK
+			if Input.is_action_just_pressed("mid_attack"):
+				playback.travel("MidAttack")
+				current_state = states.ATTACK
+			if Input.is_action_just_pressed("low_attack"):
+				playback.travel("LowAttack")
+				current_state = states.ATTACK
+			# TODO Remove this test code
+			if Input.is_action_just_pressed("damage"):
+				playback.travel("Damage")
+				current_state = states.HURT
+			if Input.is_action_pressed("move_left"):
+				playback.travel("MoveBack")
+				current_state = states.MOVE
+			if Input.is_action_pressed("move_right"):
+				playback.travel("MoveForward")
+				current_state = states.MOVE
+		states.MOVE:
+#			if Input.is_action_just_pressed("hi_attack"):
+#				playback.travel("HighAttack")
+#				current_state = states.ATTACK
+#			if Input.is_action_just_pressed("mid_attack"):
+#				playback.travel("MidAttack")
+#				current_state = states.ATTACK
+#			if Input.is_action_just_pressed("low_attack"):
+#				playback.travel("LowAttack")
+#				current_state = states.ATTACK
+			if Input.is_action_pressed("move_left"):
+				playback.travel("MoveBack")
+				# Already in MOVE state
+			if Input.is_action_pressed("move_right"):
+				playback.travel("MoveForward")
+				# Already in MOVE state
+			if Input.is_action_just_released("move_right"):
+				playback.travel("Idle")
+				current_state = states.IDLE
+			if Input.is_action_just_released("move_left"):
+				playback.travel("Idle")
+				current_state = states.IDLE
 
 func _physics_process(delta):
-	motion.y += GRAVITY
-
-	if Input.is_action_pressed("move_right"):
-		motion.x = SPEED
-	elif Input.is_action_pressed("move_left"):
-		motion.x = -SPEED
-	else:
-		motion.x = 0
-
-	if is_on_floor():
-		if Input.is_action_just_pressed("jump"):
-			motion.y = JUMP_HEIGHT
+	match current_state:
+		states.IDLE:
+			if Input.is_action_just_pressed("jump"):
+				motion.y = JUMP_HEIGHT
+				current_state = states.AIRBORNE
+				playback.travel("Jump")
+		states.AIRBORNE:
+			motion.y += GRAVITY
+			if is_on_floor():
+				playback.travel("Land")
+				current_state = states.IDLE
+		states.MOVE:
+			if Input.is_action_pressed("move_right"):
+				motion.x = SPEED
+			elif Input.is_action_pressed("move_left"):
+				motion.x = -SPEED
+			else:
+				motion.x = 0
 
 	motion = move_and_slide(motion, UP)
